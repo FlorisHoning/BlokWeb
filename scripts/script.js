df6c26b6fde1958a200fdd6644a947851c6ef3d0
FlorisHoning <70574500+FlorisHoning@users.noreply.github.com>
2020-09-30 19:07:00 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 // JavaScript Document
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+"use strict";
+
+//header dropdown
+
+var hamburgerMenu = document.querySelector('#icon');
+var dropdownMenu = document.querySelector('.dropdown');
+
+hamburgerMenu.addEventListener('click', showDropdown);
+
+function showDropdown() {
+    console.log(showDropdown);
+    dropdownMenu.classList.toggle('show');
+
+}
