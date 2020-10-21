024530d7defb8f1fe7d766e9ee2a5aeacc1568c6
FlorisHoning <70574500+FlorisHoning@users.noreply.github.com>
2020-10-21 16:56:00 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,38 @@ function showDropdown() {
     dropdownMenu.classList.toggle('show');
 
 }
+
+//details dropdown
+
+var detailsMenu = document.querySelector('.details');
+var detailsButton = document.querySelector('#detailbutton');
+
+function showDetailsdiv() {
+    console.log(showDetailsdiv);
+    detailsMenu.classList.toggle('showDetails');
+
+    var plusMinSign = document.querySelector("#plusmin");
+    plusMinSign.textContent = " - ";
+
+}
+detailsButton.addEventListener('click', showDetailsdiv);
+
+
+//responsive searchbalk laten komen
+
+var searchButton = document.querySelector('.searchglassWhite');
+var searchBalk = document.querySelector('.searchBalk2');
+var blackWhite = document.querySelector('header');
+
+
+function showSearchbalk() {
+    console.log(showSearchbalk);
+    searchBalk.classList.toggle('toonSearch');
+    searchButton.classList.toggle('toonSearchglass');
+
+    blackWhite.classList.toggle('moveheader');
+
+}
+
+
+searchButton.addEventListener('click', showSearchbalk);
